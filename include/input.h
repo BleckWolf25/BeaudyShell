a71e5eec8eef81db6e4d0b4b313910d3744820f9
BleckWolf25 <joao.coutinho08@icloud.com>
2025-03-11 19:27:30 +0000

    input.h update

--- include/input.h
+++ include/input.h
@@ -1,11 +1,30 @@
-// input.h - Input handling
-#ifndef INPUT_H
-#define INPUT_H
+// /include/input.h - Input handling
+#pragma once
 
-#include "beaudyShell.h"
-
+/**
+ * Read a line of input from standard input
+ * 
+ * @return Dynamically allocated string containing the input line with newline removed,
+ *         or NULL on EOF or error. Caller must free this memory.
+ */
 char *read_input(void);
+
+/**
+ * Parse an input string into an array of arguments
+ * 
+ * @param input Input string to parse
+ * @param arg_count Pointer to an integer where the number of arguments will be stored
+ * @return Dynamically allocated array of argument strings, or NULL on error.
+ *         The array itself must be freed by the caller, but not the individual strings
+ *         as they point into the original input string.
+ */
 char **parse_input(char *input, int *arg_count);
+
+/**
+ * Free the memory allocated for an argument array
+ * 
+ * @param args Argument array to free
+ */
 void free_args(char **args);
 
-#endif /* INPUT_H */
+/* INPUT_H */
