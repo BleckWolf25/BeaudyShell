17760a98e61c3e598df154eb54edf3cf3fc790e6
BleckWolf25 <joao.coutinho08@icloud.com>
2025-03-11 19:51:02 +0000

    updated signals.h and added empty code of conduct

--- include/signals.h
+++ include/signals.h
@@ -1,11 +1,14 @@
 // /include/signals.h - Signal handling
 #pragma once
 
+#include "beaudyshell.h"
+
 /**
  * Handle SIGCHLD signal (child process status changed)
  * 
  * @param sig Signal number
  */
+
 void handle_sigchld(int sig);
 
 /**
@@ -17,8 +20,10 @@ void handle_sigint(int sig);
 
 /**
  * Set up signal handlers for the shell
+ *
+ * @param state Shell state containing job list
  */
-void setup_signals(void);
+void setup_signals(ShellState *state);
 
 /**
  * Reset signal handlers to default behavior (for child processes)
