82b26d566a548eea65b91495a30ef9eacfd70263
BleckWolf25 <joao.coutinho08@icloud.com>
2025-03-11 19:26:46 +0000

    beaudyshell update

--- include/beaudyShell.h
+++ include/beaudyShell.h
@@ -1,17 +1,23 @@
-// beaudyShell.h - Main header file
-#ifndef BeaudyShell_H
-#define BeaudyShell_H
+// /include/beaudyshell.h - Main header file for BeaudyShell
+#pragma once
 
 #include <stdbool.h>
+#include <sys/types.h>
 
-#define MAX_INPUT_SIZE 1024
-#define MAX_ARGS 64
-#define MAX_PATH_LEN 256
-#define MAX_HOSTNAME_LEN 64
-#define MAX_USERNAME_LEN 32
-#define MAX_PROMPT_LEN 512
+// Constants
+#define MAX_INPUT_SIZE 1024      // Maximum size of input line
+#define MAX_ARGS 64              // Maximum number of command arguments
+#define MAX_PATH_LEN 256         // Maximum path length
+#define MAX_HOSTNAME_LEN 64      // Maximum hostname length
+#define MAX_USERNAME_LEN 32      // Maximum username length
+#define MAX_PROMPT_LEN 512       // Maximum prompt length
 
-// Shell state structure
+// Forward declarations
+typedef struct JobList JobList;
+
+/**
+ * Structure containing the shell's state
+ */
 typedef struct {
   char cwd[MAX_PATH_LEN];          // Current working directory
   bool running;                    // Shell running state
@@ -20,12 +26,29 @@ typedef struct {
   int last_exit_code;              // Exit code of last command
   char **environ;                  // Environment variables
   bool interactive;                // Running in interactive mode?
+  JobList *jobs;                   // Job control list
 } ShellState;
 
-// Function prototypes for main shell operations
+/**
+ * Initialize the shell state
+ * 
+ * @param state Pointer to ShellState structure to initialize
+ * @param environ Environment variables array
+ */
 void initialize_shell(ShellState *state, char **environ);
-void cleanup_shell(ShellState *state);
-void run_shell(ShellState *state);
-void handle_signal(int signo);
 
-#endif /* BEAUDYSHELL_H */
+/**
+ * Clean up resources used by the shell
+ * 
+ * @param state Pointer to ShellState structure to clean up
+ */
+void cleanup_shell(ShellState *state);
+
+/**
+ * Run the main shell loop
+ * 
+ * @param state Pointer to ShellState structure
+ */
+void run_shell(ShellState *state);
+
+/* BEAUDYSHELL_H */
