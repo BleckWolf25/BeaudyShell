4574f41ab3e67f8294687b9c330c6d2eb0ebf952
BleckWolf25 <joao.coutinho08@icloud.com>
2025-03-11 19:27:19 +0000

    execute.h update

--- include/execute.h
+++ include/execute.h
@@ -1,11 +1,34 @@
-// execute.h - Command execution
-#ifndef EXECUTE_H
-#define EXECUTE_H
+// /include/execute.h - Command execution
+#pragma once
 
-#include "beaudyShell.h"
+#include "beaudyshell.h"
 
+/**
+ * Execute a command with the given arguments
+ * 
+ * @param args Array of command arguments (args[0] is the command name)
+ * @param arg_count Number of arguments in the args array
+ * @param state Current shell state
+ * @return Exit status of the command (0 for success, non-zero for failure)
+ */
 int execute_command(char **args, int arg_count, ShellState *state);
+
+/**
+ * Handle execution of a built-in command
+ * 
+ * @param args Array of command arguments (args[0] is the command name)
+ * @param arg_count Number of arguments in the args array
+ * @param state Current shell state
+ * @return Exit status of the built-in command (0 for success, non-zero for failure)
+ */
 int handle_builtin(char **args, int arg_count, ShellState *state);
+
+/**
+ * Check if a command is a built-in command
+ * 
+ * @param cmd Command name to check
+ * @return true if the command is a built-in command, false otherwise
+ */
 bool is_builtin(char *cmd);
 
-#endif /* EXECUTE_H */
+/* EXECUTE_H */
