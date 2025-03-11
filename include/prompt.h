9f0d1bcc69e9a00ae9cb6f6504501677041db174
BleckWolf25 <joao.coutinho08@icloud.com>
2025-03-11 19:28:34 +0000

    prompt.h update

--- include/prompt.h
+++ include/prompt.h
@@ -1,10 +1,20 @@
-// prompt.h - Shell prompt
-#ifndef PROMPT_H
-#define PROMPT_H
+// /include/prompt.h - Shell prompt
+#pragma once
 
-#include "beaudyShell.h"
+#include "beaudyshell.h"
 
+/**
+ * Print the shell prompt to standard output
+ * 
+ * @param state Current shell state containing prompt information
+ */
 void print_prompt(ShellState *state);
+
+/**
+ * Update prompt information in the shell state
+ * 
+ * @param state Current shell state to update
+ */
 void update_prompt_info(ShellState *state);
 
-#endif /* PROMPT_H */
+/* PROMPT_H */
